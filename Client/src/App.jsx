7b1ef5935c2677a84d5e13956a6c09f1c81269fc
John Patrick Trinidad <Ptrinidad765@gmail.com>
2025-06-03 10:37:05 +0800

    initial commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <div className=''>s</div>
+    </main>
+  )
+}
+
+export default App
